9a246d1e7d12ee501c79ff3a867a57f0cba75bbc
Shima <shima.jin@icloud.com>
2018-07-08 19:08:27 +0900

    clean

---
+++ DailyiOSAnimations/Day5ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Day5ViewController.swift
+//  DailyiOSAnimations
+//
+//  Created by Jinsei Shima on 2018/07/08.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class Day5ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

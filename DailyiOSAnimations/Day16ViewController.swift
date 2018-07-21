348cda1abce132f54ec119544d0c619952726f93
Shima <shima.jin@icloud.com>
2018-07-22 04:00:25 +0900

    Update day16

--- DailyiOSAnimations/Day16ViewController.swift
+++ DailyiOSAnimations/Day16ViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class Day16ViewController: UIViewController {
 
+    let label = CountUpLabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        view.backgroundColor = .white
+
+        label.frame = CGRect(x: 0, y: 0, width: 120, height: 60)
+        label.textAlignment = .center
+        label.center = view.center
+        view.addSubview(label)
+
+        label.text = "count up label"
+
+        let button = UIButton()
+        button.setTitle("Count Up", for: .normal)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 120, height: 60)
+        button.center = CGPoint(x: view.center.x, y: view.center.y + 120)
+        button.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
+        view.addSubview(button)
+
+    }
+
+    @objc func tapButton(_ sender: UIControl) {
+        label.count(from: 10, to: 100, duration: 2)
     }
 }
 
@@ -22,17 +44,57 @@ class CountUpLabel: UILabel {
 
     var timer: CADisplayLink?
 
-    func count(from: Int, to: Int, duration: CGFloat) {
+    var progress: TimeInterval = 0
+    var lastUpdate: TimeInterval = 0
+    var totalTime: TimeInterval = 0
 
-        timer?.invalidate()
-        timer = nil
+    var fromValue: CGFloat = 0
+    var toValue: CGFloat = 0
+
+    var currentTime: TimeInterval {
+        if progress >= totalTime { return TimeInterval(toValue) }
+        return TimeInterval(fromValue) + (progress / totalTime) * TimeInterval(toValue - fromValue)
+    }
+
+    let completion: () -> Void = {}
+
+    func count(from: CGFloat, to: CGFloat, duration: TimeInterval) {
+
+        reset()
+
+        fromValue = from
+        toValue = to
 
         timer = CADisplayLink(target: self, selector: #selector(updateValue(_:)))
         timer?.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
         timer?.add(to: RunLoop.main, forMode: RunLoopMode.UITrackingRunLoopMode)
+
+        totalTime = duration
+        lastUpdate = Date.timeIntervalSinceReferenceDate
+    }
+
+    func reset() {
+
+        timer?.invalidate()
+        timer = nil
+
+        progress = 0
+        lastUpdate = 0
+        totalTime = 0
     }
 
     @objc func updateValue(_ timer: Timer) {
+
+        let now = Date.timeIntervalSinceReferenceDate
+        progress += now - lastUpdate
+        lastUpdate = now
+
+        if progress >= totalTime {
+            print("completion")
+            reset()
+        }
+
+        text = String(format: "%.0f", currentTime)
 
     }
 }

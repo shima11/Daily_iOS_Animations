e5331ae39572f4e2a0b145e36927b5b42bc153b5
Shima <shima.jin@icloud.com>
2018-07-24 09:29:28 +0900

    Update day17

--- DailyiOSAnimations/Day17ViewController.swift
+++ DailyiOSAnimations/Day17ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-// https://github.com/muukii/Play.PresentationController/tree/master/PresentationController
-
 class Day17ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        view.backgroundColor = .white
+
+        let emitterLayer = CAEmitterLayer()
+        emitterLayer.emitterPosition = view.center
+        emitterLayer.emitterShape = kCAEmitterLayerLine
+        emitterLayer.emitterSize = view.bounds.size
+
+        let emitterCell = CAEmitterCell()
+        emitterCell.birthRate = 100
+        emitterCell.lifetime = 3
+        emitterCell.velocity = 40
+        emitterCell.scale = 0.2
+        emitterCell.emissionRange = CGFloat.pi * 2
+        emitterCell.color = UIColor.darkGray.cgColor
+
+        let target = UIView()
+        target.backgroundColor = .darkGray
+
+        target.frame = CGRect(x: 0, y: 0, width: 8, height: 32)
+        UIGraphicsBeginImageContextWithOptions(target.frame.size, true, 1)
+        target.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        emitterCell.contents = image?.cgImage
+
+        emitterLayer.emitterCells = [emitterCell]
+
+        view.layer.addSublayer(emitterLayer)
+
     }
 }
